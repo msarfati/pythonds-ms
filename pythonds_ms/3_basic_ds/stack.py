8dcf7101f59846d6404f9e50849faccce7e24363
Michael Sarfati <michael.sarfati@utoronto.ca>
2016-05-21 12:36:07 -0400

    testing for Stack class

--- pythonds_ms/3_basic_ds/stack.py
+++ pythonds_ms/3_basic_ds/stack.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python
 # Impl. of a Stack
+from unittest import TestCase
+
+class EmptyStack(Exception):
+    def __str__(self):
+        return "You cannot pop from an empty stack."
 
 class Stack():
     def __init__(self, *args):
-        self.stack = [*args]
+        self.__stack__ = [*args]
 
-#    def __repr__(self):
-#        return self.stack
+    def __repr__(self):
+        return str(self.__stack__)
+
+    def __len__(self):
+        return self.__stack__.__len__()
+
+    def push(self, item):
+        self.__stack__.append(item)
+
+    def pop(self):
+        try:
+            return self.__stack__.pop()
+        except:
+            raise EmptyStack
+
+    def peek(self):
+        return self.__stack__[-1]
+
+
+def StackTestCase(TestCase):
+   def test_pop():
+        s = Stack(3, "sum", 3.2)
+        for i in range(len(s)):
+            s.pop()
+        s.pop()
+
