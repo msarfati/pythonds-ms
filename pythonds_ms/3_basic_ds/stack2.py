b5146feebb0a9c8cec946dcba615a6821b505bd6
Michael Sarfati <michael.sarfati@utoronto.ca>
2016-10-01 19:54:21 -0400

    function to convert arbitrary decimal integer into arbitary base + test OK

--- pythonds_ms/3_basic_ds/stack2.py
+++ pythonds_ms/3_basic_ds/stack2.py
@@ -44,15 +44,15 @@ def test_balancedParens():
 
 
 def divideBy2(v: int) -> str:
+    "A method for converting decimal to binary."
     assert v > 0, "Must be greater than zero."
     remainders = Stack()
-    i = v
-    while i > 0:
-        remainders.push(i % 2)
-        i //= 2
+    while v > 0:
+        remainders.push(v % 2)
+        v //= 2
 
     bin_str = "0b"
-    for i in range(remainders.size()):
+    while not remainders.isEmpty():
         bin_str += str(remainders.pop())
 
     return bin_str
@@ -64,9 +64,36 @@ def test_divideBy2():
     assert divideBy2(35) != bin(99)
 
 
+def divideByN(v: int, base: int) -> str:
+    "A method for converting any decimal number into any base."
+    assert v > 0, "Must be greater than zero."
+    remainders = Stack()
+    while v > 0:
+        remainders.push(v % base)
+        v //= base
+
+    converted_str = ""
+    from string import ascii_lowercase
+    num_map = {k+10: v for k, v in zip(range(26), ascii_lowercase)}
+    while not remainders.isEmpty():
+        remainder = remainders.pop()
+        if remainder > 9:
+            remainder = num_map[remainder]
+        converted_str += str(remainder)
+
+    return converted_str
+
+
+def test_divideByN():
+    assert divideByN(255, 16) == hex(255)[2:]
+    assert divideByN(34, 8) == oct(34)[2:]
+    assert divideByN(42, 2) == bin(42)[2:]
+
+
 def main():
     # test_balancedParens()
-    test_divideBy2()
+    # test_divideBy2()
+    test_divideByN()
 
 if __name__ == '__main__':
     main()
